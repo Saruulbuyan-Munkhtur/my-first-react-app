2cec644066ab4b63aabc2e0b322a02f1b26803fd
ErozionZeal <48438567+ErozionZeal@users.noreply.github.com>
2020-06-29 11:31:11 +0800

    Clock DOM update example

--- src/index.js
+++ src/index.js
@@ -4,12 +4,41 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// jsdrops.com/react-dom2
+
+const render = () => {
+  document.getElementById('mountNode').innerHTML = `
+    <div>
+      Hello HTML
+      <input />
+      <pre>${new Date().toLocaleTimeString()}</pre>
+    </div>
+  `;
+
+  ReactDOM.render(
+    React.createElement(
+      'div',
+      null,
+      'Hello React ',
+      React.createElement('input', null),
+      React.createElement(
+        'pre',
+        null,
+        new Date().toLocaleTimeString()
+      )
+    ),
+    document.getElementById('mountNode2')
+  );
+};
+setInterval(render, 1000);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
