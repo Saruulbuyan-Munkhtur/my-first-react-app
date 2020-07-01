aefb6f4e9d1263572567599a1b64ceb09eaff6b4
ErozionZeal <48438567+ErozionZeal@users.noreply.github.com>
2020-07-01 12:14:29 +0800

    Added moments time

--- src/components/Body/Timer_Date.js
+++ src/components/Body/Timer_Date.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Clock from 'react-clock';
+import Moment from 'react-moment';
+// import 'moment-timezone';
 
 export default class Navbar extends React.Component {
     render() {
         return (
             <div className='col-lg'>
                 <Clock />
+                <Moment />
             </div>
         );
     }
