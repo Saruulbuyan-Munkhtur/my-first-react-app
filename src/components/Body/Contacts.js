083be2ea0b3257185985fa6425cc9ea2633cc274
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-07-09 23:34:36 +0800

    😃 Learnt how to use state to make a working serachbox for the contactList!

--- src/components/Body/Contacts.js
+++ src/components/Body/Contacts.js
@@ -2,34 +2,47 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import CardList from './ContactList/CardList';
 import { contacts } from './ContactList/contacts';
+import SearchBox from './ContactList/SearchBox';
 
-
-export default class Contacts extends React.Component {
+class Contacts extends React.Component {
   constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+    super();
+    // this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      contacts: contacts,
+      searchfield: ''
+    }    
   }
   
-  handleClick() {
-    alert('Click happened');
+  // handleClick() {
+  //   alert('Click happened');
+  // }
+
+  onSearchChange = (event) => {
+    this.setState({'searchfield': event.target.value})
   }
 
   
   render() {
-        return (
-            <div className="container">
-              <h1 className="text-primary tc">Friends</h1>
-              <div className="tc flex flex-wrap">
-                <input id="to-do-input" class="input-reset ba b--black-20 pa2 mb2 db w-40 br3" type="text" aria-describedby="name-desc"></input>
-                <button type="button" className="ph2 pv1 mh3 btn btn-success" id="to-do-submit">Search</button>
-              </div>
-              <div className="flex flex-wrap bg-light-gray">
-                <CardList contacts={contacts}/>
-              </div>
-              <div className="container-fluid">
-                <button type="button" className="btn btn-success" id="to-do-refresh" onClick={this.handleClick}>Refresh List</button>
-              </div>
+    const filteredContacts = this.state.contacts.filter(contacts => {
+      return contacts.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+      return (
+          <div className="container">
+            <h1 className="text-primary tc">Friends</h1>
+            <div className="tc">
+              <SearchBox searchChange={this.onSearchChange}/>
+              {/* <button type="button" className="ph2 pv1 mh3 btn btn-success" id="to-do-submit">Search</button> */}
             </div>
-        );
+            <div className="flex flex-wrap bg-light-gray">
+              <CardList contacts={filteredContacts}/>
+            </div>
+            <div className="container-fluid">
+              <button type="button" className="btn btn-success" id="to-do-refresh" onClick={this.handleClick}>Refresh List</button>
+            </div>
+          </div>
+      );
     }
 }
+
+export default Contacts;
--- src/components/Body/ToDoList.js
+++ src/components/Body/ToDoList.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import ListItem from './ToDoList/ListItem';
+import SearchBow from './ContactList/SearchBox';
 
 export default class ToDoList extends React.Component {
 
 	constructor(props){
 		super(props)
 	}
-	// const addItem = () => {
+	
 
-	// }
-	// let to-do-list = document.getElementById('to-do-list');
-    render() {
-        return (
-            <div class="list-group-item">
-				<h1 class="text-primary">To do list</h1>
-				<div class="row">
-					<input type="text" id="to-do-input"></input>
-					<button type="button" class="btn btn-success" id="to-do-submit">Submit</button>
-				</div>
-				<div class="container-fluid border-dark">
-					<ul id="to-do-list" class="flex flex-column text-secondary list pl0">
-						<ListItem content="First" id={'listContent'}/>
-						<ListItem content="Second" />
-						<ListItem content="Third" />
-					</ul>
-				</div>
-				<div class="container-fluid">
-					<button type="button" class="btn btn-success" id="to-do-refresh">Refresh List</button>
-				</div>
+	render() {
+			return (
+					<div class="list-group-item">
+			<h1 class="text-primary">To do list</h1>
+			<div class="row">
+				<input type="text" id="to-do-input"></input>
+				<button type="button" class="btn btn-success" id="to-do-submit">Submit</button>
 			</div>
-        );
-    }
+			<div class="container-fluid border-dark">
+				<ul id="to-do-list" class="flex flex-column text-secondary list pl0">
+					<ListItem content="First" id={'listContent'}/>
+					<ListItem content="Second" />
+					<ListItem content="Third" />
+				</ul>
+			</div>
+			<div class="container-fluid">
+				<button type="button" class="btn btn-success" id="to-do-refresh">Refresh List</button>
+			</div>
+		</div>
+			);
+	}
 }
