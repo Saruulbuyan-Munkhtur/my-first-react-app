b4103bacc01e60fd385fdefb46b08a420b0f4079
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-07-16 09:56:21 +0800

    Updates

---
+++ precache-manifest.a2294d50adffed60655824b83d2452f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c786ed8d7ea0ec68c91c5ae3920171",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "d9e92fb83bb6e0f357bb",
+    "url": "/my-first-react-app/static/css/2.f20ff292.chunk.css"
+  },
+  {
+    "revision": "6d507c8053a53ddf60b3",
+    "url": "/my-first-react-app/static/css/main.c9a4f38a.chunk.css"
+  },
+  {
+    "revision": "d9e92fb83bb6e0f357bb",
+    "url": "/my-first-react-app/static/js/2.0d22b55b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.0d22b55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d507c8053a53ddf60b3",
+    "url": "/my-first-react-app/static/js/main.730983d8.chunk.js"
+  },
+  {
+    "revision": "e4bd278056ab3ce325f3",
+    "url": "/my-first-react-app/static/js/runtime-main.ae42f839.js"
+  }
+]);
